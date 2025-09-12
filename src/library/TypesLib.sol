c2a90f1021c334e83fa0a4e6795202b21d5bf15d
LanreAkintayo <akintayolanre2019@gmail.com>
2025-09-12 19:51:02 +0100

    updated

--- src/library/TypesLib.sol
+++ src/library/TypesLib.sol
@@ -21,4 +21,87 @@ library TypesLib {
         Status status;
         uint256 id;
     }
+
+
+     //////////////////////////
+    // ENUMS
+    //////////////////////////
+
+    enum EvidenceType {
+        TEXT,
+        IMAGE,
+        VIDEO,
+        AUDIO,
+        DOCUMENT
+    }
+
+    //////////////////////////
+    // STRUCTS
+    //////////////////////////
+
+    struct Evidence {
+        uint256 dealId;
+        address uploader;
+        string uri;
+        uint256 timestamp;
+        EvidenceType evidenceType;
+        string description;
+        bool removed;
+    }
+
+    struct Dispute {
+        address initiator;
+        address sender;
+        address receiver;
+        address winner;
+        uint256 dealId;
+        uint256 disputeFee;
+        address feeTokenAddress;
+    }
+
+    struct Juror {
+        address jurorAddress;
+        uint256 stakeAmount;
+        uint256 reputation;
+        uint256 missedVotesCount;
+        uint256 lastWithdrawn;
+    }
+
+    // Keeps track of the stake amount and reputation at selection
+    struct Candidate {
+        uint256 disputeId;
+        address jurorAddress;
+        uint256 stakeAmount;
+        uint256 reputation;
+        uint256 score;
+        bool missed;
+    }
+
+    struct RequestStatus {
+        uint256 paid; // Amount paid in LINK
+        bool fulfilled; // Whether request was successfully fulfilled
+        uint256[] randomWords;
+    }
+
+    struct Vote {
+        address jurorAddress;
+        uint256 disputeId;
+        uint256 dealId;
+        address support;
+    }
+
+    struct Timer {
+        uint256 disputeId;
+        uint256 startTime;
+        uint256 standardVotingDuration;
+        uint256 extendDuration;
+    }
+
+    struct PaymentType {
+        uint256 disputeId;
+        address tokenAddress;
+        uint256 amount;
+    }
+
+
 }
